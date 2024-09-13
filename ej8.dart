73276e96e488b562904094395efaa68134a0451c
Diego <garciadiee_@hotmail.com>
2024-09-12 21:14:27 -0300

    banco pro

--- ej8.dart
+++ ej8.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class Cuenta {
 //Definimos propiedades
   String titular;
@@ -7,17 +9,23 @@ class Cuenta {
   Cuenta(this.titular, this.saldo);
 
 //Para depositar
-  void depositar(double monto) {
-    saldo += monto;
+  void depositar() {
+    print('Cuanto va a depositar?');
+    String? monto = stdin.readLineSync()!;
+    int montoParse = int.parse('$monto');
+    saldo += montoParse;
     print('\$$monto depositado con exito. Su saldo actual es de \$$saldo');
   }
 
 //Para retirar
-  void retirar(double retiro) {
-    if (retiro > saldo) {
+  void retirar() {
+    print('Cuanto va a retirar?');
+    String? retiro = stdin.readLineSync()!;
+    int retiroParse = int.parse('$retiro');
+    if (retiroParse > saldo) {
       print('Fondos insuficientes. Tu saldo es de \$$saldo');
     } else {
-      saldo -= retiro;
+      saldo -= retiroParse;
       print('\$$retiro retirados con exito. Su saldo actual es de \$$saldo');
     }
   }
@@ -27,21 +35,64 @@ class Cuenta {
     print('Titular: $titular');
     print('Saldo: \$$saldo');
   }
+
 }
 
 void main() {
-  //Cuenta bancaria
+  bool continuar = true;
+
+   //Cuenta bancaria
   Cuenta cuenta1 = Cuenta('Diego', 100000.50);
+//Mostramos el menu hasta que el usuario quiera salir
+  while (continuar) {
+    menu();
+    print('Selecciona una opcion:');
+    int opcion = int.parse(stdin.readLineSync()!);
 
-  //Mostramos
-  cuenta1.mostrar();
+    switch (opcion) {
+      case 1:
+        cuenta1.mostrar();
+        break;
+      case 2:
+        cuenta1.depositar();
+        break;
+      case 3:
+        cuenta1.retirar();
+        break;
+      case 4:
+        continuar = false;
+        print('Nos vemos...');
+        break;
+      default:
+        print('Opcion invalida. Presta atencion');
+    }
+  }
+  }
+    void menu() {
+    print('\nMI CUENTA');
+    print('1. Consultar saldo');
+    print('2. Depositar');
+    print('3. Retirar');
+    print('4. Salir');
+  }
 
-  //Retirar mas del saldo
-  cuenta1.retirar(205420.66);
 
-  //Retirar un monto acorde al saldo
-  cuenta1.retirar(50000.50);
 
-  //Resumen
-  cuenta1.mostrar();
-}
+
+// /*
+// void main() {
+//   //Cuenta bancaria
+//   Cuenta cuenta1 = Cuenta('Diego', 100000.50);
+
+//   //Mostramos
+//   cuenta1.mostrar();
+
+//   //Retirar mas del saldo
+//   cuenta1.retirar(205420.66);
+
+//   //Retirar un monto acorde al saldo
+//   cuenta1.retirar(50000.50);
+
+//   //Resumen
+//   cuenta1.mostrar();
+// }
